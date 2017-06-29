2190bfbe9ae9cca0cb72badbc4983e9ba84c0ab6
Denttt <tyurinden@ya.ru>
2017-06-30 00:39:31 +0400

    несколько изменена струкура кода, добавлены комментарии

--- src/gb/level3/lesson3/J3Lesson_3_HW_2.java
+++ src/gb/level3/lesson3/J3Lesson_3_HW_2.java
@@ -11,37 +11,41 @@ public class J3Lesson_3_HW_2 {
         Set<InputStream> inputStreamSet = new LinkedHashSet<>();
         SequenceInputStream seqInputStream;
         Enumeration<InputStream> inputStreamsEnum;
-
         File someDir = new File("SOMEDIR");
         File outputFile = new File(someDir, "result.txt");
+
+        //@TODO Надо ли сообщения об ошибках заменить на константы?
+        if (!someDir.exists()) {
+            System.out.println("ERROR: The specified folder not found!");
+            return;
+        }
         try {
-            outputFile.createNewFile();
-        } catch (IOException e1) {
-            System.out.println("An error occurred while creating the output file!");
-            e1.printStackTrace();
+            outputFile.createNewFile();//@TODO War: Result of 'File.createNewFile()' is ignored
+        } catch (IOException e) {
+            System.out.println("ERROR: An error occurred while creating the output file!");
+            e.printStackTrace();
         }
         if (!someDir.isDirectory()) {
-            System.out.println(someDir.getName() + " is not a folder or it does not exist."
+            System.out.println("ERROR: " + someDir.getName() + " is not a folder."
                     + " Specify the correct folder name!");
             return;
         }
-        if (someDir.list().length == 0) {
-            System.out.println("The specified folder is empty!");
+        if (someDir.list().length == 0) { //@TODO War: Dereference of 'someDir.list()' may produce 'java.lang.NullPointerException'
+            System.out.println("ERROR: The specified folder is empty!");
             return;
         }
         System.out.println(Arrays.deepToString(someDir.list()));
-        for (File file: someDir.listFiles()) {
+        for (File file: someDir.listFiles()) { //@TODO War: Dereference of 'someDir.listFiles()' may produce 'java.lang.NullPointerException'
             if (file.isDirectory()) {
                 System.out.println("DIR: " + file.getName());
             } else fileListSet.add(file);
         }
         System.out.println(fileListSet);
-        Iterator<File> fileIterator = fileListSet.iterator();
-        while (fileIterator.hasNext()) {
+        for (File aFileListSet : fileListSet) { //DONE! //@TODO War: 'while' loop replaceable with 'foreach'
             try {
-                inputStreamSet.add(new BufferedInputStream(new FileInputStream(fileIterator.next())));
+                inputStreamSet.add(new BufferedInputStream(new FileInputStream(aFileListSet)));
             } catch (FileNotFoundException e) {
-                System.out.println("Some files are locked or missing or an HDD error!");
+                System.out.println("ERROR: Some files are locked or missing or an HDD error!");
                 e.printStackTrace();
             }
         }
@@ -53,21 +57,21 @@ public class J3Lesson_3_HW_2 {
                 try {
                     bos.write(bArr, 0, bytesRead);
                 } catch (IOException e) {
-                    System.out.println("An error occurred while writing the output file!");
+                    System.out.println("ERROR: An error occurred while writing the output file!");
                     e.printStackTrace();
                 }
                 bytesRead = seqInputStream.read(bArr);
             }
         } catch (IOException e) {
-            System.out.println("Some problems with the output file or stream or an HDD error!");
+            System.out.println("ERROR: Some problems with the output file or stream or an HDD error!");
             e.printStackTrace();
         }
         try {
             seqInputStream.close();
         } catch (IOException e) {
-            System.out.println("An error occurred while closing the output file!");
+            System.out.println("ERROR: An error occurred while closing the output file!");
             e.printStackTrace();
         }
-        System.out.println("Operation complete!");
+        System.out.println("OK: Operation complete!");
     }//main
 } //class
