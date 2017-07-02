d98e41b3fa52c08439e1fb2d40c5942bf9e0dcb5
Denttt <tyurinden@ya.ru>
2017-07-02 19:24:27 +0400

    избавился от байтового массива для чтения. не совсем понимаю как, но работает. как потоки обмениваются данными?

--- src/gb/level3/lesson3/J3Lesson_3_HW_2.java
+++ src/gb/level3/lesson3/J3Lesson_3_HW_2.java
@@ -7,8 +7,6 @@ public class J3Lesson_3_HW_2 {
 
     public static void main(String[] args) {
 //        byte bArr[] = new byte[40];
-//        byte b = 0;
-//        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
         Set<File> fileListSet = new LinkedHashSet<>();
         Set<InputStream> inputStreamSet = new LinkedHashSet<>();
         SequenceInputStream seqInputStream;
@@ -21,27 +19,32 @@ public class J3Lesson_3_HW_2 {
             System.out.println("ERROR: The specified folder not found!");
             return;
         }
+
         try {
             outputFile.createNewFile();//@TODO War: Result of 'File.createNewFile()' is ignored
         } catch (IOException e) {
             System.out.println("ERROR: An error occurred while creating the output file!");
             e.printStackTrace();
         }
+
         if (!someDir.isDirectory()) {
             System.out.println("ERROR: " + someDir.getName() + " is not a folder."
                     + " Specify the correct folder name!");
             return;
         }
+
         if (someDir.list().length == 0) { //@TODO War: Dereference of 'someDir.list()' may produce 'java.lang.NullPointerException'
             System.out.println("ERROR: The specified folder is empty!");
             return;
         }
+
         System.out.println(Arrays.deepToString(someDir.list()));
         for (File file: someDir.listFiles()) { //@TODO War: Dereference of 'someDir.listFiles()' may produce 'java.lang.NullPointerException'
             if (file.isDirectory()) {
                 System.out.println("DIR: " + file.getName());
             } else fileListSet.add(file);
         }
+
         System.out.println(fileListSet);
         for (File aFileListSet : fileListSet) { //DONE! //@TODO War: 'while' loop replaceable with 'foreach'
             try {
@@ -51,6 +54,7 @@ public class J3Lesson_3_HW_2 {
                 e.printStackTrace();
             }
         }
+
         inputStreamsEnum = Collections.enumeration(inputStreamSet);
         seqInputStream = new SequenceInputStream(inputStreamsEnum);
         try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(outputFile))) {
@@ -72,6 +76,7 @@ public class J3Lesson_3_HW_2 {
             System.out.println("ERROR: Some problems with the output file or stream or an HDD error!");
             e.printStackTrace();
         }
+
         try {
             seqInputStream.close();
         } catch (IOException e) {
@@ -79,10 +84,5 @@ public class J3Lesson_3_HW_2 {
             e.printStackTrace();
         }
         System.out.println("OK: Operation complete!");
-//        try {
-//            byteOutputStream.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     } //main
 } //class
